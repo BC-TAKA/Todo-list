e5290760c20a5c2a5db1013b8d9e24722b0acc0f
raveger <73781361+raveger@users.noreply.github.com>
2020-11-04 12:36:30 +0900

    Add files via upload

---
+++ ToDo/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	)
+
+type TodoData struct {
+	Id int 'json:"id"'
+	Name string 'json:"name"'
+	Todo string 'json:"todo"'
+}
+
+func main() {
+	
+}
