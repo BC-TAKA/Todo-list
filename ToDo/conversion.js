5664476ac1da0aa4594c9d0736e8a94eff85da2b
raveger <ytakada0109@gmail.com>
2020-11-09 14:18:53 +0900

    アドバイスを反映

--- ToDo/conversion.js
+++ ToDo/conversion.js
@@ -3,12 +3,17 @@ fetch('http://localhost:8081/todos')
     console.log(response.status); // => 200
     return response.json().then(userInfo => {
         // JSONパースされたオブジェクトが渡される
-        userInfo.forEach(element => {
-            Object.entries(element).forEach(([key, val]) => {
-                console.log(key, val)
+        const parent = document.getElementById("parent")
+        userInfo.forEach(info => {
+            const aTag = document.createElement("a")
+
+            Object.entries(info).forEach(([key, val]) => {
+                const aTag = document.createElement("span")
+                span.innerText = `${key}: ${val}`
+                aTag.appendChild(span)
+                aTag.appendChild(document.createElement("br"))
             });
         });
-
         // console.log(userInfo); // => {...} userInfoにデコード済みのものが入っている
         // userInfo.forEach(element => {
         //     console.log(Object.entries(element))
