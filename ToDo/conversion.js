6c16ed1efae04b91403623e004f068ae3d6a4408
raveger <ytakada0109@gmail.com>
2020-11-09 16:57:49 +0900

    16:57更新分

--- ToDo/conversion.js
+++ ToDo/conversion.js
@@ -3,14 +3,15 @@ fetch('http://localhost:8081/todos')
     console.log(response.status); // => 200
     return response.json().then(userInfo => {
         // JSONパースされたオブジェクトが渡される
-        var parent = document.getElementById("parent")
+        const parent = document.getElementById("parent")
         userInfo.forEach(info => {
-            var aTag = document.createElement("a")
+            const aTag = document.createElement("a")
 
             Object.entries(info).forEach(([key, val]) => {
-                var span = document.createElement("span")
+                const span = document.createElement("span")
                 span.innerText = `${key}: ${val}`
                 aTag.appendChild(span)
+                aTag.href = "#"
             });
             parent.appendChild(aTag)
             parent.appendChild(document.createElement("br"))
