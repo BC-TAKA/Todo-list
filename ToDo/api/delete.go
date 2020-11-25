8fa6e5a8bffd97fdf88c7a6b6d32d64ddcd7bfc1
raveger <ytakada0109@gmail.com>
2020-11-25 09:12:34 +0900

    11/24 レビュー(2)修正

--- ToDo/api/delete.go
+++ ToDo/api/delete.go
@@ -4,7 +4,7 @@ import "database/sql"
 
 // DELETE処理を行う関数
 func DeleteTODO(id int, DB *sql.DB) error {
-	_, err := DB.Exec("DELETE FROM todolist WHERE id= ?", id)
+	_, err := DB.Exec("DELETE FROM todolist WHERE id = ?", id)
 	if err != nil {
 		return err
 	}
--- ToDo/api/update.go
+++ ToDo/api/update.go
@@ -9,7 +9,7 @@ import (
 // UPDATE処理を行う関数
 func UpdateTODO(todo model.TODO, DB *sql.DB) error {
 	_, err := DB.Exec(
-		`UPDATE todolist SET name= ?, TODO= ? WHERE id= ?`,
+		`UPDATE todolist SET name = ?, TODO = ? WHERE id = ?`,
 		todo.Name, todo.Todo, todo.ID,
 	)
 	if err != nil {
