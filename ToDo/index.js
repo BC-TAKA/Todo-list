35545dbd5805ddf6e113f30ef238c27e20bc37a9
raveger <ytakada0109@gmail.com>
2020-11-27 17:22:06 +0900

    17:22レビュー修正

--- ToDo/index.js
+++ ToDo/index.js
@@ -2,16 +2,16 @@ var app = new Vue ({
     el: '#app',
     data: {
         todos: [],
-        create: {},
-        update: {},
+        createTodoForm: {},
+        updateTodoForm: {},
     },
     created() {
         this.getTODOs()
     },
     methods: {
         //一覧表示機能
-        async getTODOs() {
-            await axios.get('http://localhost:8081/todos')
+        getTODOs() {
+            axios.get('http://localhost:8081/todos')
             .then((response) => {
                 this.todos = response.data
             }).catch((error) => {
@@ -19,31 +19,31 @@ var app = new Vue ({
             })
         },
         //新規登録機能
-        async doAdd() {
-            await axios.post('http://localhost:8081/todos', this.create)
+        doAdd() {
+            axios.post('http://localhost:8081/todos', this.createTodoForm)
             .then((response) => {
                 alert("登録完了しました。")
                 this.getTODOs()
-                this.create = {}
+                this.createTodoForm = {}
             }).catch((error) => {
                 console.log(error);
             })
         },
         //更新機能
-        async doUpdate() {
-            await axios.put('http://localhost:8081/todos', this.update)
+        doUpdate() {
+            axios.put('http://localhost:8081/todos', this.updateTodoForm)
             .then((response) => {
                 alert("更新が完了しました。")
                 this.getTODOs()
-                this.update = {}
+                this.updateTodoForm = {}
             }).catch((error) => {
                 console.log(error);
             })
         },
         //削除機能
-        async doRemove(todo) {
+        doRemove(todo) {
             const id = todo.ID
-            await axios.delete(`http://localhost:8081/todos?id=${id}`)
+            axios.delete(`http://localhost:8081/todos?id=${id}`)
             .then((response) => {
                 this.getTODOs()
             }).catch((error) => {
